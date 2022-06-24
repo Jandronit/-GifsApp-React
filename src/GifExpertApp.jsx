4ae003b1b53f50c051df3f43db5f3f185a793eb3
Jandronit <jandrohen@gmail.com>
2022-06-24 19:54:41 +0200

    (lesson 75 course react) Create one list categories with Hooks

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,9 +1,28 @@
+import { useState } from 'react';
 
 export const GifExpertApp = () => {
+
+    const [ categories, setCategories ] = useState([ 'One Punch', 'Samurai X', 'Dragon Ball' ]);
+
+    console.log(categories);
     return (
         <>
-            
+            {/* titulo */}
             <h1>GifExpertApp</h1>
+
+            {/* Input */}
+
+            {/* Listado de Gif */}
+            <ol>
+                {
+                    categories.map(category => {
+                    return <li key={category}>{category}</li>
+                })
+                }
+            </ol>
+                {/* Gif Item */}
+
+
         </>
     );
 }
