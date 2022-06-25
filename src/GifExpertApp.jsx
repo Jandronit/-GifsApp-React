372190b2600dadb20a4a327479fb84fd6b35f775
Jandronit <jandrohen@gmail.com>
2022-06-25 14:28:23 +0200

    (lesson 79 course react) validate that names are unique

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -5,8 +5,11 @@ export const GifExpertApp = () => {
 
     const [ categories, setCategories ] = useState([ 'One Punch', 'Samurai X', 'Dragon Ball' ]);
 
-    const onAddCategories = (e) => {
-        setCategories([e, ...categories ]);
+    const onAddCategories = (newCategory) => {
+
+        if ( categories.includes(newCategory) ) return;
+
+        setCategories([newCategory, ...categories ]);
     }
 
     return (
