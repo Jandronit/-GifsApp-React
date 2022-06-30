7925b72ba317ebcd1d8dfc296e993874908bbdd2
Alejandro Arguello <jandrohen@gmail.com>
2022-06-30 18:19:42 +0200

    (lesson 102 course react) Implements tests good running the GifItem component

--- tests/components/GifItem.test.jsx
+++ tests/components/GifItem.test.jsx
@@ -1,4 +1,4 @@
-import { render } from '@testing-library/react';
+import {render, screen} from '@testing-library/react';
 import { GifItem } from '../../src/components';
 
 describe('Pruebas en <GifItem/>', () => {
@@ -11,6 +11,22 @@ describe('Pruebas en <GifItem/>', () => {
         const { container } = render( <GifItem title={ title } url={ url } /> );
         expect(container).toMatchSnapshot();
 
+    });
+
+    test('Debe de mostrar la imagen con el URL y el ALT indicado', () => {
+
+        render( <GifItem title={ title } url={ url } /> );
+        // expect( screen.getByRole('img').src ).toBe( url );
+        // expect( screen.getByRole('img').alt ).toBe( title );
+        const { src, alt } = screen.getByRole('img');
+        expect( src ).toBe( url );
+        expect( alt ).toBe( title );
+    });
+
+    test('Debe de mostrar el titulo con el texto indicado', () => {
+
+            render( <GifItem title={ title } url={ url } /> );
+            expect( screen.getByText(title) ).toBeTruthy();
     });
 
 });
