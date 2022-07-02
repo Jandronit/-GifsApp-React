33ac1e5d36d18777809cdcd4f14126c85b1a3d2f
Alejandro Arguello <jandrohen@gmail.com>
2022-07-02 20:18:48 +0200

    (lesson 104 course react) Add test in AddCategory components

---
+++ tests/hooks/AddCategory.test.jsx
@@ -0,0 +1,15 @@
+import {fireEvent, render, screen} from '@testing-library/react';
+import {AddCategory} from '../../src/components';
+
+describe('Pruebas en <AddCategory/>', () => {
+
+    test('debe de cambiar el valor de la caja de texto', () => {
+
+        render(<AddCategory onNewCategory={ () => {} } />);
+        const input = screen.getByRole('textbox');
+
+        fireEvent.input( input, { target: { value: 'test' } } );
+
+        expect( input.value ).toBe('test');
+    });
+});
